6a6fe6ef40aa3ceeb83ff9666c3021fa2a752463
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-04-29 16:27:29 +0900

    MovieList 컴포넌트 처음 마운트 할 때 인기있는 영화 불러오게 해주기

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -1,15 +1,27 @@
-import React, { VFC } from 'react';
+import React, { VFC, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../reducers';
 import { Grid, Typography } from '@material-ui/core';
 import MovieCard from '../MovieCard/MovieCard';
 import { useStyles } from './styles';
+import { useDispatch } from 'react-redux';
+import * as api from '../../api';
+import { setMovies } from '../../actions';
 
 const MovieList: VFC = () => {
 	const classes = useStyles();
+	const dispatch = useDispatch();
 	const movies = useSelector((state: RootState) => {
 		return state.movies?.data;
 	});
+
+	useEffect(() => {
+		async function fetchMovies() {
+			const data = await api.getPopularMovies();
+			dispatch(setMovies(data));
+		}
+		fetchMovies();
+	}, [dispatch]);
 
 	return (
 		<div className={classes.root}>
